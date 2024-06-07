0684009389d0758a9aa52984986524ec339733a0
INFOR\matteou <matteourso100@gmail.com>
2024-06-07 14:32:38 +0200

    Special Offer

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,36 @@
 type Props = {
   label: string;
   iconUrl?: string;
+  backgroundColor?: string;
+  borderColor?: string;
+  textColor?: string;
+  fullWidth?: boolean;
 };
 
-const Button = ({ label, iconUrl }: Props) => {
+const Button = ({
+  label,
+  iconUrl,
+  backgroundColor,
+  borderColor,
+  textColor,
+  fullWidth,
+}: Props) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white border-coral-red"
+      } rounded-full ${fullWidth && "w-full"}`}
+    >
       {label}
+
       {iconUrl && (
         <img
           src={iconUrl}
-          alt="Arrow right"
-          className="ml-2 rounded-full w-5 h-5"
+          alt="arrow right icon"
+          className="ml-2 rounded-full bg-white w-5 h-5"
         />
       )}
     </button>
