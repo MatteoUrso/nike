8e0dd2da3f13fcc433697f976bb8ccca16d4045a
INFOR\matteou <matteourso100@gmail.com>
2024-06-07 11:45:12 +0200

    Hero Section

---
+++ src/sections/index.ts
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomersReviews from "./CustomersReviews";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  CustomersReviews,
+  Footer,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+};
